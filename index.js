50435ffd766462243227c79e2bcab418ee00e6dd
Daniel Lockyer <hi@daniellockyer.com>
2020-08-18 19:38:56 +0100

    Fixed exporting of helper function

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 module.exports = function (shipit) {
     require('./lib/deploy')(shipit);
 };
+
+module.exports.getServerList = function () {
+    let deployUser = process.env.DEPLOY_USER;
+    let deployServers = process.env.DEPLOY_SERVERS;
+
+    try {
+        let parsedServers = JSON.parse(deployServers);
+        return parsedServers.map(item => deployUser + '@' + item);
+    } catch (e) {
+        return deployUser + '@' + deployServers;
+    }
+};
