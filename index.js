d1e83b1c83c9d65233faa0c7e14797b41493c050
Sam Lord <hello@samlord.me>
2026-03-23 16:27:08 +0000

    Swapped var for let/const

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ module.exports = function (shipit) {
 };
 
 module.exports.getServerList = function () {
-    let deployUser = process.env.DEPLOY_USER;
-    let deployServers = process.env.DEPLOY_SERVERS;
+    const deployUser = process.env.DEPLOY_USER;
+    const deployServers = process.env.DEPLOY_SERVERS;
 
     try {
-        let parsedServers = JSON.parse(deployServers);
+        const parsedServers = JSON.parse(deployServers);
         return parsedServers.map(item => deployUser + '@' + item);
     } catch (e) {
         return deployUser + '@' + deployServers;
